5ac2c9de34b6cb95b1e314929877b2b245e42fc9
Mamlik <57287362+Mamlik@users.noreply.github.com>
2022-02-10 22:28:03 +0300

    markdown_pre_added

--- consts.py
+++ consts.py
@@ -2,3 +2,5 @@ geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 delta = "0.005"
 map_api_server = "http://static-maps.yandex.ru/1.x/"
 FORMATS = {'map': 'png', 'sat': 'jpg', 'sat,skl': 'jpg'}
+map_move_speed = 0.001
+delta = '90'
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import sys
 from PyQt5 import uic, QtCore
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox
 import requests
-from consts import map_api_server, FORMATS
+from consts import map_api_server, FORMATS, map_move_speed, delta, geocoder_api_server
 from PyQt5.QtCore import Qt
-import datetime
 
 
 class MainWindow(QMainWindow):
@@ -15,70 +14,60 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('YandexMaps')
         self.toponym_longitude, self.toponym_lattitude = "37.617734", "55.752004"
         self.map_layer = "map"
-        self.z = 15
-        # Добавил размер шага для смещения изображения
-        self.step = 0.3
+        self.delta = delta
         self.show_map()
+        self.sheme_btn.clicked.connect(self.layer_clicked)
+        self.sat_btn.clicked.connect(self.layer_clicked)
+        self.hyb_btn.clicked.connect(self.layer_clicked)
+        self.search_btn.clicked.connect(self.search_adress)
 
     def show_map(self):
         map_params = {
             "ll": ",".join([self.toponym_longitude, self.toponym_lattitude]),
-            "z": self.z,
+            "spn": ",".join([self.delta, self.delta]),
             "l": self.map_layer,
         }
         response = requests.get(map_api_server, params=map_params)
         if not response:
-            #  todo cause connection exception
-            pass
+            self.internet_connection_error()
         pixmap = QPixmap()
         pixmap.loadFromData(QtCore.QByteArray(response.content), FORMATS[self.map_layer])
         self.img_lbl.setPixmap(pixmap)
-        self.sheme_btn.clicked.connect(self.layer_clicked)
-        self.sat_btn.clicked.connect(self.layer_clicked)
-        self.hyb_btn.clicked.connect(self.layer_clicked)
 
     def keyPressEvent(self, event):
-
-        # Заменил названия обрабатываемых клавиш
-        if event.key() == Qt.Key_PageUp:
-            self.z += 1
-            if self.z < 0 or self.z > 19:
-                self.z -= 1
-            self.show_map()
-
-        elif event.key() == Qt.Key_PageDown:
-            self.z -= 1
-            if self.z < 0 or self.z > 19:
-                self.z += 1
-            self.show_map()
-
-
-        # Добавил обработку кнопок для смещения и кнопку выхода
-        elif event.key() == Qt.Key_Right:
-            self.toponym_longitude = str(float(self.toponym_longitude) + self.step)
-            self.show_map()
-
-        elif event.key() == Qt.Key_Left:
-            self.toponym_longitude = str(float(self.toponym_longitude) - self.step)
-            self.show_map()
-
-        elif event.key() == Qt.Key_Up:
-            self.toponym_lattitude = str(float(self.toponym_lattitude) + self.step)
+        if event.key() == Qt.Key_Up:
+            self.delta = str(float(self.delta) / 2)
+            if float(self.delta) < 0.0009:
+                self.delta = '0.0009'
             self.show_map()
 
         elif event.key() == Qt.Key_Down:
-            self.toponym_lattitude = str(float(self.toponym_lattitude) - self.step)
+            self.delta = str(float(self.delta) * 2)
+            if float(self.delta) > 50:
+                self.delta = '50'
             self.show_map()
 
-        elif event.key() == Qt.Key_Escape:
-            exit()
+        elif event.key() == Qt.Key_D:
+            self.toponym_longitude = str(float(self.toponym_longitude) + float(self.delta))  # СДВИГ ПРОИСХОДИТ
+            # НЕ НА ШИРИНУ ЭКРАНА, А НА ПОЛОВИНУ ЭКРАНА ДЛЯ УДОБСТВА
+            self.show_map()
 
+        elif event.key() == Qt.Key_A:
+            self.toponym_longitude = str(float(self.toponym_longitude) - float(self.delta))  # СДВИГ ПРОИСХОДИТ
+            # НЕ НА ШИРИНУ ЭКРАНА, А НА ПОЛОВИНУ ЭКРАНА ДЛЯ УДОБСТВА
+            self.show_map()
 
+        elif event.key() == Qt.Key_W:
+            self.toponym_lattitude = str(float(self.toponym_lattitude) + float(self.delta))  # СДВИГ ПРОИСХОДИТ
+            # НЕ НА ШИРИНУ ЭКРАНА, А НА ПОЛОВИНУ ЭКРАНА ДЛЯ УДОБСТВА
+            self.show_map()
+
+        elif event.key() == Qt.Key_S:
+            self.toponym_lattitude = str(float(self.toponym_lattitude) - float(self.delta))
+            self.show_map()
 
     def layer_clicked(self):
         button = QApplication.instance().sender()
-        print(button.text())
-
         if button.text() == 'Схема':
             self.map_layer = 'map'
             self.show_map()
@@ -90,6 +79,40 @@ class MainWindow(QMainWindow):
         if button.text() == 'Гибрид':
             self.map_layer = 'sat,skl'
             self.show_map()
+
+    def search_adress(self):
+        user_request = self.adress_edit.text()
+        geocoder_params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+            "geocode": user_request,
+            "format": "json"}
+        response = requests.get(geocoder_api_server, params=geocoder_params)
+        if not response:
+            self.internet_connection_error()
+        json_response = response.json()
+        try:
+            toponym = json_response["response"]["GeoObjectCollection"][
+                "featureMember"][0]["GeoObject"]
+            toponym_coodrinates = toponym["Point"]["pos"]
+            toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+            self.toponym_longitude, self.toponym_lattitude = toponym_longitude, toponym_lattitude
+            self.show_map()
+        except IndexError:
+            self.object_not_found_error()
+
+    def internet_connection_error(self):
+        msg = QMessageBox()
+        msg.setWindowTitle("Ошибка")
+        msg.setText("Интернет соединение не найдено")
+        msg.setIcon(QMessageBox.Warning)
+        msg.exec_()
+
+    def object_not_found_error(self):
+        msg = QMessageBox()
+        msg.setWindowTitle("Ошибка")
+        msg.setText("Объект не найден")
+        msg.setIcon(QMessageBox.Information)
+        msg.exec_()
 
 
 if __name__ == '__main__':
